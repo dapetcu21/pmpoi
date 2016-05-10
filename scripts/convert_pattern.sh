ebc476169db0af0eeaf728ed43773ac019bbad41
Marius Petcu <dapetcu21@gmail.com>
2016-05-10 14:32:11 +0300

    Change structure of pattern header

--- scripts/convert_pattern.sh
+++ scripts/convert_pattern.sh
@@ -6,7 +6,7 @@ then
   exit 1
 fi
 
-function round_hex {
+function trunc_hex {
   DEC=$((16#$1))
   ROUND=$((($DEC + 8) / 16))
   if [[ "$ROUND" == 16 ]]; then ROUND=15; fi
@@ -14,29 +14,34 @@ function round_hex {
 }
 
 DATA=$(\
-gm convert $1 -rotate 90 rgb:- |\
+gm convert $1 -rotate 90 rgb:- 2> /dev/null |\
 hexdump -v -e '6/1 "%02x " "\n"' |\
 while read -r -a line
 do
-  R1=$(round_hex ${line[0]})
-  G1=$(round_hex ${line[1]})
-  B1=$(round_hex ${line[2]})
-  R2=$(round_hex ${line[3]})
-  G2=$(round_hex ${line[4]})
-  B2=$(round_hex ${line[5]})
+  R1=$(trunc_hex ${line[0]})
+  G1=$(trunc_hex ${line[1]})
+  B1=$(trunc_hex ${line[2]})
+  R2=$(trunc_hex ${line[3]})
+  G2=$(trunc_hex ${line[4]})
+  B2=$(trunc_hex ${line[5]})
   printf "0x%x%x, 0x%x%x, 0x%x%x, " $R1 $G1 $B1 $R2 $G2 $B2
 done |\
 sed 's/, $//' \
 )
 
 META=$(exiftool -Description -b "$1")
+SAMPLES=$(gm identify -format %w "$1" 2> /dev/null)
 
 cat > "${1}.h" << EOM
 #pragma once
-#include "../pattern_image.h"
 
-struct PatternImageData pattern_$(basename $1 .png) = {
+struct {
+    unsigned char samples;
+    float radiansPerPixel;
+    unsigned char data[$SAMPLES * 12];
+} pattern_arrow = {
+  .samples = $SAMPLES,
 $(echo -n "${META}" | sed 's/^/  /')
-  .data = [${DATA}]
+  .data = { ${DATA} }
 };
 EOM
